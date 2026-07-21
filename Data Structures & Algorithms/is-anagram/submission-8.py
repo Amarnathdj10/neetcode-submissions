52bee952c5a1dbc522973c8ec99026bcf5f332e5
Amarnath D.J. <amarnathdj56@gmail.com>
2026-07-21 16:30:32 +0530

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hashmap_s, hashmap_t = {}, {}
+        for char in s:
+            hashmap_s[char] = hashmap_s.get(char,0)+1
+        for char in t:
+            hashmap_t[char] = hashmap_t.get(char,0)+1
+        return hashmap_s == hashmap_t
